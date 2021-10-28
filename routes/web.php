a2769595cfb87cce17e12d81f1062886c635f43c
Fathir Ahmad <fadiahahmad597@gmail.com>
2021-10-29 01:12:17 +0700

    Added: Logout route

--- routes/web.php
+++ routes/web.php
@@ -10,4 +10,8 @@ Route::get('/login', [AuthController::class, 'loginView'])->name('login.loginVie
 Route::get('/register', [AuthController::class, 'registerView'])->name('register.registerView');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
-Route::get('/dashboard', []);
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::middleware(['auth'])->group(function() {
+    Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
+});
